7781710b502086b3707021ac7fa9ccbfb94e6b68
naufalMAG <nofalalgi123@gmail.com>
2023-10-06 16:38:36 +0700

    mengumpulkan tugas pertemuan 3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya naufal muhammad alghifari<br>
+              saya tinggal di bekasi daerah bekasi utara<br>
+              olahraga yang saya sukai adalah Basket";
+    }
+}
